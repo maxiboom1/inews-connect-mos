cded4f71f8d569a6b0d8bbcecc2089db93b2bd33
Alex Samih-zade <galutziogen@gmail.com>
2026-03-17 11:39:28 +0200

    start messages edited

--- src/3-utilities/logger-messages.js
+++ src/3-utilities/logger-messages.js
@@ -8,7 +8,7 @@ class Messages {
         const host = getServerIP();
         console.clear();
         logger(`**********************************************************************`,"blue");
-        logger(`[SYSTEM] Inews-Connect, App Version: ${appConfig.version}, now starting...`,"green");
+        logger(`[SYSTEM] SegevSport Inews-Connect service, ver: ${appConfig.version}, now starting...`,"green");
         logger(`[SYSTEM] Plugin url: http://${host}:${appConfig.pluginPort}/index.html`,"green")
         logger(`**********************************************************************`,"blue");
     }
